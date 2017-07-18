b6094e2d51fbdda7bcb25496cc13315d29b7271c
designens <inyoung524@naver.com>
2017-07-18 17:51:32 +0900

    LNB 메뉴 스크립트 적용

--- markup/assets/js/common.js
+++ markup/assets/js/common.js
@@ -58,5 +58,32 @@
        $('#gnb-all-menu').toggleClass('open');
     });
 
+    $(function() {
+        var Accordion = function(el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
+
+            // Variables privadas
+            var links = this.el.find('.lnb-link');
+            // Evento
+            links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+        }
+
+        Accordion.prototype.dropdown = function(e) {
+            var $el = e.data.el;
+                $this = $(this),
+                $next = $this.next();
+
+            $next.slideToggle();
+            $this.parent().toggleClass('open');
+
+            if (!e.data.multiple) {
+                $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+            }
+
+        };
+
+        var accordion = new Accordion($('#accordion'), false);
+    });
 
 })(window, document, window.jQuery);
