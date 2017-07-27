2865562150e1209c2085f6b9d76977df9afec0f1
designens <inyoung524@naver.com>
2017-07-27 09:59:20 +0900

    탭 메뉴 스크립트 수정, 탭 영역 마크업 재수정

--- public/assets/js/lib-js.js
+++ public/assets/js/lib-js.js
@@ -329,10 +329,11 @@ window.smoothScroll = (function (window, document, undefined) {
 }(window.jQuery));
 ;(function($) {
 
-    var tabs = $(".tablist li[role='tab']"),
+    var tabs = $(".tablist a[role='tab']"),
         panel = $(".tab-panel[role='tabpanel']");
 
-    tabs.click(function() {
+    tabs.click(function(ev) {
+        ev.preventDefault(); 
         tabs.attr("aria-selected", "false");
         $(this).attr("aria-selected", "true");
         var tabpanid = $(this).attr("aria-controls");
@@ -347,14 +348,27 @@ window.smoothScroll = (function (window, document, undefined) {
         }
     });
 
-    tabs.keydown(function(ev) { 
-        if ((ev.which == 39) || (ev.which == 37)) {
-            var selected = $(this).attr("aria-selected");
-            if (selected == "true") {
-                $(".tablist li[aria-selected='false']").attr("aria-selected","true").focus();
-                $(this).attr("aria-selected","false");
-                var tabpanid = $(".tablist li[aria-selected='true']").attr("aria-controls");
-                var tabpan = $("#" + tabpanid);
+    tabs.keydown(function(ev) {
+        ev.preventDefault(); 
+        var _this, selected, index, length, tab, tabpanid, tabpan;
+        var key = ev.keyCode;
+        var dir = 0;
+        var left = key === 37;
+        var right = key === 39;
+        if (left) { dir = -1; }
+        if (right){ dir = 1;  }
+        if (left || right) {
+            _this = $(this);
+            selected = _this.attr("aria-selected");
+            if (selected === "true") {
+                tabs.filter("[aria-selected='true']").attr("aria-selected","false");
+                index = _this.index() + dir;
+                length = tabs.length;
+                if ( index < 0 ) { index = length - 1; }
+                if ( index >= length ) { index = 0; }
+                tab = tabs.eq(index).attr("aria-selected", "true").focus();
+                tabpanid = tab.attr("aria-controls");
+                tabpan = $("#" + tabpanid);
                 panel.attr("aria-hidden", "true");
                 tabpan.attr("aria-hidden", "false");
             }
